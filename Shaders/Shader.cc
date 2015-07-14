9b8a59c4ebc0ebfb2a6d621444b6d1e8e495c542
Robin Seibold <robinbobseibold@gmail.com>
2015-07-14 16:26:08 +0200

    Add Shader class used to initiate Shaders

--- Shaders/Shader.cc
+++ Shaders/Shader.cc
@@ -1,1 +1,84 @@
-x
+
+#include "Shader.h"
+
+Shader::Shader(std::string shaderName) {
+    const char *vertexShaderSourceCode = readSourceCode("Shaders/" + shaderName + ".vert").c_str();
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSourceCode, NULL);
+    glCompileShader(vertexShader);
+    checkCompilationSuccess(vertexShader);
+    
+    const char *fragmentShaderSourceCode = readSourceCode("Shaders/" + shaderName + ".frag").c_str();
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSourceCode, NULL);
+    glCompileShader(fragmentShader);
+    checkCompilationSuccess(fragmentShader);
+    
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkLinkingSuccess(shaderProgram);
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+void Shader::activate() {
+    glUseProgram(shaderProgram);
+}
+
+void Shader::unactivate() {
+    glUseProgram(0);
+}
+
+std::string Shader::readSourceCode(std::string fileName) {
+    std::string sourceCode;
+    std::ifstream inputFile(fileName);
+    if (inputFile) {
+        inputFile.seekg(0, std::ios::end);
+        sourceCode.reserve(inputFile.tellg());
+        inputFile.seekg(0, std::ios::beg);
+    
+        sourceCode.assign((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+    } else {
+        std::cout << "Shader file not found" << std::endl;
+    }
+    return sourceCode;
+}
+
+void Shader::checkCompilationSuccess(GLuint shader) {
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar log[512];
+        glGetShaderInfoLog(shader, 512, NULL, log);
+        std::cout << "Shader compilation failed: \n" << log << std::endl;
+    }
+}
+
+void Shader::checkLinkingSuccess(GLuint program) {
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        GLchar log[512];
+        glGetProgramInfoLog(program, 512, NULL, log);
+        std::cout << "Program linkage failed: \n" << log << std::endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Shaders/Shader.h
+++ Shaders/Shader.h
@@ -1,1 +1,22 @@
-x
+
+#include <openGL/gl3.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class Shader {
+    
+public:
+    Shader(std::string shaderName);
+    void activate();
+    void unactivate();
+    
+private:
+    GLuint shaderProgram;
+    
+    std::string readSourceCode(std::string fileName);
+    void checkCompilationSuccess(GLuint shader);
+    void checkLinkingSuccess(GLuint program);
+    
+};
