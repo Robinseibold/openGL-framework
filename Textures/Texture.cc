211462d8b45fc4794ab18a114260bb2b3bcdb6d4
Robin Seibold <robinbobseibold@gmail.com>
2015-07-16 13:29:25 +0200

    Add Texture class used to initilize 2D textures for now

---
+++ Textures/Texture.cc
@@ -0,0 +1,38 @@
+
+#include "Texture.h"
+
+Texture::Texture() {
+    glGenTextures(1, &texture);
+}
+
+Texture2D::Texture2D(std::string textureName) {
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    SDL_Surface *image = IMG_Load(("Textures/" + textureName + ".png").c_str());
+    if (image) {
+        width = image->w;
+        height = image->h;
+    
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->pixels);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        SDL_FreeSurface(image);
+    } else {
+        std::cout << "Could not initilize Texture: " << IMG_GetError() << std::endl;
+    }
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::activate() {
+    glBindTexture(GL_TEXTURE_2D, texture);
+}
+
+void Texture2D::unactivate() {
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
---
+++ Textures/Texture.h
@@ -0,0 +1,31 @@
+
+#include <openGL/gl3.h>
+#include <SDL_image/SDL_image.h>
+
+#include <string>
+#include <iostream>
+
+class Texture {
+    
+public:
+    Texture();
+    virtual void activate() = 0;
+    virtual void unactivate() = 0;
+    
+protected:
+    GLuint texture;
+    int width, height;
+    
+};
+
+
+class Texture2D : public Texture {
+    
+public:
+    Texture2D(std::string textureName);
+    void activate();
+    void unactivate();
+    
+};
+
+
