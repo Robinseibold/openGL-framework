c01f5d4f457cc6f9bd5bf7c20c7bb11185f9af06
Robin Seibold <robinbobseibold@gmail.com>
2015-08-03 11:21:16 +0200

    Add EBO and destructor

--- Objects/Object.h
+++ Objects/Object.h
@@ -1,16 +1,30 @@
 
+#include <openGL/gl3.h>
+#include <vector>
+
+#include "../Shaders/Shader.h"
+
 class Object {
     
 public:
     Object() {
-        glGenBuffers(1, &vertexBufferObject);
         glGenVertexArrays(1, &vertexArrayObject);
+        glGenBuffers(1, &vertexBufferObject);
+        glGenBuffers(1, &elementBufferObject);
+    }
+    virtual ~Object() {
+        glDeleteVertexArrays(1, &vertexArrayObject);
+        glDeleteBuffers(1, &vertexBufferObject);
+        glDeleteBuffers(1, &elementBufferObject);
     }
     virtual void draw() = 0;
     
-    
 protected:
-    GLuint vertexBufferObject;
     GLuint vertexArrayObject;
+    GLuint vertexBufferObject;
+    GLuint elementBufferObject;
+    
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
     
 };
